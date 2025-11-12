7be2c7af1383d220d05b2a4e5de34e4330929ee7
devteam-Altrai <devteam@altrai.in>
2025-11-12 11:50:56 +0530

    Register feature update and bug fix.

--- src/utils/AppConstant.js
+++ src/utils/AppConstant.js
@@ -1,6 +1,6 @@
-// const AUTH_URL = "https://saralaquotation-v2.onrender.com/api/auth/";
+// const AUTH_URL = "http://127.0.0.1:8000/api/auth/";
 const AUTH_URL = "https://saralaquote.saralaengineering.in/api/auth/";
-// const BASE_URL = "https://saralaquotation-v2.onrender.com/uploader/";
+// const BASE_URL = "http://127.0.0.1:8000/uploader/";
 const BASE_URL = "https://saralaquote.saralaengineering.in/uploader/";
 const SPI_URL =
   "https://saralaengineering.sharepoint.com/sites/Sarala_Engineering";
